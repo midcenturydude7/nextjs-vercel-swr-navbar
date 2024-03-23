b41ed16313d349d8567ed9841ff08c912450510a
midCenturyDude7 <mgriffes@gmail.com>
2024-03-23 15:50:33 -0700

    Set up usePathname() for selected tab

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,34 +1,57 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
-
-const links = [
-  {
-    id: Math.random(),
-    name: "Docs",
-    path: "/docs",
-  },
-  {
-    id: Math.random(),
-    name: "Examples",
-    path: "/examples",
-  },
-  {
-    id: Math.random(),
-    name: "Blog",
-    path: "/blog",
-  },
-];
+import { cn } from "../../lib/utils";
 
 export default function Header() {
+  const links = [
+    {
+      id: Math.random(),
+      name: "Docs",
+      path: "/docs",
+    },
+    {
+      id: Math.random(),
+      name: "Examples",
+      path: "/examples",
+    },
+    {
+      id: Math.random(),
+      name: "Blog",
+      path: "/blog",
+    },
+  ];
+
+  const pathname = usePathname();
+
   return (
     <div className="flex h-14 w-full items-center justify-between border-b-[1px] border-b-zinc-500 bg-zinc-900 pl-[20%] pr-[20%]">
       <ul>
-        <li>Logo</li>
+        <li>
+          <Link
+            className={cn(
+              "relative rounded-md px-4 py-2 font-medium transition-all duration-500 ease-out hover:bg-slate-200",
+              pathname === "/" ? "cursor-default bg-slate-300" : "",
+            )}
+            href="/"
+          >
+            Logo
+          </Link>
+        </li>
       </ul>
       <ul className="flex w-[30%] justify-evenly">
         {links.map(({ id, name, path }) => (
           <li key={id}>
-            <Link href={path}>{name}</Link>
+            <Link
+              className={cn(
+                "relative rounded-md px-4 py-2 font-medium transition-all duration-500 ease-out hover:bg-slate-200",
+                pathname === path ? "cursor-default bg-slate-300" : "",
+              )}
+              href={path}
+            >
+              {name}
+            </Link>
           </li>
         ))}
       </ul>
